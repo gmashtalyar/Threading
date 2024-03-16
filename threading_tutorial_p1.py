71041897b9ff52563fe543c445f4ea4f36d631ef
gmashtalyar <gmashtalyar@yandex.ru>
2024-03-16 16:46:35 +0300

    basic tutorial, see notes

---
+++ threading_tutorial_p1.py
@@ -0,0 +1,21 @@
+import threading
+import time
+
+
+def func():
+    print('ran')
+    time.sleep(1)
+    print('done')
+    time.sleep(0.85)
+    print("now done")
+
+
+x = threading.Thread(target=func)
+x.start()
+
+print(threading.active_count())
+time.sleep(0.9)
+print('finally')
+
+
+
